78b574d95acf560592df475e69f39790659b1bfd
Martijn Velders <martijn.velders@kramp.com>
2017-11-04 22:00:55 +0100

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ const config = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'assets/js/[name].bundle.js'
-    },
+    }, 
     module: {
         rules: [
             {
